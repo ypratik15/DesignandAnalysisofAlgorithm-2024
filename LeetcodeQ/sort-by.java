f4b213a014c18f165dc912656cbbb33c5ac371b9
ypratik15 <12202040601047@adit.ac.in>
2024-11-13 08:02:02 +0530

    Create sort-by.java

---
+++ LeetcodeQ/sort-by.java
@@ -0,0 +1,19 @@
+/**
+ * @param {Array} arr
+ * @param {Function} fn
+ * @return {Array}
+ */
+var sortBy = function(arr, fn) {
+    return arr.sort((a, b) => {
+        const aValue = fn(a);
+        const bValue = fn(b);
+
+        if (aValue < bValue) {
+            return -1;
+        } else if (aValue > bValue) {
+            return 1;
+        } else {
+            return 0;
+        }
+    });
+};
